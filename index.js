f678947d83b09598e60a8bbd704b120055407750
limonvigo <116071830+limonvigo@users.noreply.github.com>
2022-10-22 22:07:53 +0200

    committ initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+
